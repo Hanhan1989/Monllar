6a8d8e42b35fd165b0d1caa28c6da180809cf5d8
aldo1 <sivilavaquiata@gmail.com>
2018-05-10 18:38:33 +0200

    Columnas de productos.

--- resources/views/frontal/portada.blade.php
+++ resources/views/frontal/portada.blade.php
@@ -7,5 +7,177 @@
 @endsection
 
 @section('content')
-    cuerpo
+    <div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card mb-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>1.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto....... </p>
+
+                 <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                        <div class="button">
+                            <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>2.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto.... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>3.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto.... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>4.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>1.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto....... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>2.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto.... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>3.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto.... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card md-3 box-shadow">
+                <img class="card-img-top" src="http://via.placeholder.com/150x150" alt="Card image cap">
+                <div class="card-body">
+                    <h4>4.Nombre Producto</h4>
+                    <p class="d-flex card-text">Descripcion del producto... </p>
+
+                    <div class="text-center">
+                        <div class="precio ">PRECIO
+                            <span> €</span>
+                        </div>
+                        <div class="precio_iva small">(PRECIO
+                            <span> €</span>IVA Incluido.)
+                        </div>
+                    </div>
+                    <div class="button">
+                        <button type="button" class="btn btn-primary btn-block">Comprar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
 @endsection
