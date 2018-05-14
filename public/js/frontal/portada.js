9e69ad93cac2973490138285bc411fc4a1a5b38a
aldo1 <sivilavaquiata@gmail.com>
2018-05-14 17:20:27 +0200

    Botón de compra de los productos.

--- public/js/frontal/portada.js
+++ public/js/frontal/portada.js
@@ -1,1 +1,8 @@
-alert("hola hoal");
+//Jquery boton comprar
+$(document).ready(function mouse() {
+    $(document).on('mouseover', '.card', function () {
+        $(this).find(":button").show();
+    }).on('mouseout', '.card', function () {
+        $(this).find(":button").hide();
+    });
+});
