509ebff51c7ffeaf14ecf5abe321d31153e20257
aldo1 <sivilavaquiata@gmail.com>
2018-05-15 20:04:32 +0200

    Modal para las subscripciones

--- resources/views/layout-frontal.blade.php
+++ resources/views/layout-frontal.blade.php
@@ -15,8 +15,34 @@
     <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
             <div class="col-4 pt-1">
-                <a class="text-muted" href="#">Suscribirse</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" >Subscribirse</button>
             </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Subscripción</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="recipient-name" class="col-form-label">Email:</label>
+                                    <input type="text" class="form-control" id="recipient-name" placeholder="example@gmail.com">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                            <button type="button" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <div class="col-4 text-center">
                 <a class="blog-header-logo text-dark" href="#">Monllar</a>
             </div>
