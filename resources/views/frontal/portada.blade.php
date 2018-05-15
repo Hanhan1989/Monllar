df9cce5574877258bb0d7cbe77ff84fe50cf1651
aldo1 <sivilavaquiata@gmail.com>
2018-05-15 17:57:44 +0200

    Flexslider para poner a nuestros proveedores o poner categorias de nuestros productos

--- resources/views/frontal/portada.blade.php
+++ resources/views/frontal/portada.blade.php
@@ -228,7 +228,68 @@
             </div>
         </div>
     </div>
+      <div class="row" >
+          <div class="flex-slider">
+              <div class="container">
+                  <h2>Patrocinadores de nuestra tienda o categorias de ropa</h2>
+                  <div class="row" id="itemSlider">
+                      <div id="slider-1" class="carousel carousel-by-item slide" data-ride="carousel">
+                          <div class="carousel-inner" role="listbox">
+                              <div class="carousel-item active">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/03658c/dddddd&text=Item+1" alt="First slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/0e7373/dddddd&text=Item+2" alt="Second slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/265902/dddddd&text=Item+3" alt="Third slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/592512/dddddd&text=Item+4" alt="Fourth slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/d93d04/dddddd&text=Item+5" alt="Five slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/03658c/dddddd&text=Item+6" alt="Six slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/d93d04/dddddd&text=Item+7" alt="Seven slide">
+                                  </div>
+                              </div>
+                              <div class="carousel-item">
+                                  <div class="col-lg-2 col-md-3 col-sm-4 col-4">
+                                      <img class="d-block img-fluid" src="https://dummyimage.com/1200x800/265902/dddddd&text=Item+8" alt="Eight slide">
+                                  </div>
+                              </div>
 
+                          </div>
+                          <a class="carousel-control-prev" href="#slider-1" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#slider-1" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
 @endsection
 
 @section('javascript')
