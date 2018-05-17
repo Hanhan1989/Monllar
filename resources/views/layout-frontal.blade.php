4b12010bb73b3ae37228eeaf487afb963aac542d
Alex Chavez <alex@activitat.com>
2018-05-17 20:11:48 +0200

    #0017 Zoom Ficha Producto - Eliminado JS

--- resources/views/layout-frontal.blade.php
+++ resources/views/layout-frontal.blade.php
@@ -7,8 +7,6 @@
     <link href="{{ asset('css/comun.css') }}" rel="stylesheet">
     <link href="{{ asset('css/comun-frontal.css') }}" rel="stylesheet">
     @yield('css')
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    @yield('js')
 </head>
 <body>
 <div class="container">
