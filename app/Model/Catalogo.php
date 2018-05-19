9ec2282f7bcb24f1431d6b36d21a53cc8de0fe49
Hanhan Chen <chenhanhan1989@gmail.com>
2018-05-19 10:30:36 +0200

    sacar datos de categorias y catalogo

--- app/Model/Catalogo.php
+++ app/Model/Catalogo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Catalogo extends Model
 {
-    //
+    protected $fillable = ['id', 'nombre'];
+
+    public function categorias()
+    {
+        return $this->hasMany('App\Model\Categoria', 'id_catalogo');
+    }
 }
