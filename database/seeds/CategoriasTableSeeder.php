260b835d5d4add271860a2b7f3d61b0635c79edc
Hanhan Chen <chenhanhan1989@gmail.com>
2018-05-19 08:33:56 +0200

    eliminar la tabla cache y categoria_catalogo y modificar el seeder

--- database/seeds/CategoriasTableSeeder.php
+++ database/seeds/CategoriasTableSeeder.php
@@ -14,81 +14,97 @@ class CategoriasTableSeeder extends Seeder
         DB::table('categorias')->insert([
             [
                 'nombre' => 'abrigos',
+                'id_catalogo' => '1',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'cazadoras',
+                'id_catalogo' => '1',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'blazers',
+                'id_catalogo' => '1',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'vestidos',
+                'id_catalogo' => '1',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'bolsos',
+                'id_catalogo' => '1',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'trajes',
+                'id_catalogo' => '2',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'polos',
+                'id_catalogo' => '2',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'sudaderas',
+                'id_catalogo' => '2',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'punto',
+                'id_catalogo' => '2',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'pantalones',
+                'id_catalogo' => '2',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'niña 5-14 años',
+                'id_catalogo' => '3',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'niño 5-14 años',
+                'id_catalogo' => '3',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'accesorios',
+                'id_catalogo' => '3',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'bebé niña 3 meses - 4 años',
+                'id_catalogo' => '4',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'bebé niño 3 mesees - 4 ñiños',
+                'id_catalogo' => '4',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ],
             [
                 'nombre' => 'bebé accesorios',
+                'id_catalogo' => '4',
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]
