d1e649c879146d17d916e8f43dddc55539e38de9
Hanhan Chen <chenhanhan1989@gmail.com>
2018-05-20 14:53:02 +0200

    Preparar ficheros para listar productos. Css y javascript

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/admin/portada', 'Admin\PortadaController@index');
 Route::get('/user/login', 'Frontal\UserController@login');
 Route::get('/user/registro', 'Frontal\UserController@registrarse');
 Route::get('/producto/{producto_slug}', 'Frontal\ProductoController@show');
+Route::get('/producto/lista/{id_categoria}', 'Frontal\ProductoController@listar');
 Route::resource('admin/producto', 'Admin\ProductoController');
 
 Auth::routes();
