35663929d14c2cba2fde98235631fc00257c41ba
Hanhan Chen <chenhanhan1989@gmail.com>
2018-05-20 18:09:18 +0200

    Maquetar el login del admin

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Login Administradores</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="{{ asset('css/admin/user_login.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    @yield('content')
+
+    <script src="{{ asset('lib/validate.js') }}"></script>
+    <script src="{{ asset('js/admin/user_login.js') }}"></script>
+</body>
+</html>
