fb31c47bd2d3cc537247616cbf140f80f144b425
aldo1 <sivilavaquiata@gmail.com>
2018-05-24 20:23:36 +0200

    Modificación de la maquetación de la tabla producto, funcionalidad de confirmar el borrado de producto.

--- resources/views/admin/producto.blade.php
+++ resources/views/admin/producto.blade.php
@@ -8,37 +8,56 @@
 @endsection
 
 @section('content')
+
+
     <div class="row">
-        <table class="table table">
+        <div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <a href="{{url('admin/producto/create')}}" class="float-left btn btn-info">Crear Producto </a><br><br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row table-responsive-md">
+        <table class="table table-bordered table-hover">
             <thead>
                 <tr>
-                    <th scope="col-1">Id</th>
-                    <th scope="col-2">Nombre</th>
-                    <th scope="col-2">Descripción</th>
-                    <th scope="col-3">Imagen</th>
-                    <th scope="col-2">Created at</th>
-                    <th scope="col-1">Eliminar</th>
-                    <th scope="col-1">Edit</th>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre del producto  </th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Created at</th>
+                    <th scope="col">Eliminar</th>
+                    <th scope="col">Edit</th>
                 </tr>
             </thead>
             <tbody>
 
     @foreach($productos as $producto)
         <tr>
-            <th scope="row">{{$producto->id}}</th>
-            <td>{{$producto->nombre}}</td>
-            <td>{{ str_limit($producto->descripcion, 50, '...')}}</td>
-            <td><img class="card-img-top" src="{{$producto->path_imagen_1}}" alt="Card image cap"></td>
-            <td>{{$producto->created_at}}</td>
+            <th >{{$producto->id}}</th>
+            <td >{{$producto->nombre}}</td>
+            <td>{{ str_limit($producto->descripcion, 30, '...')}}</td>
+            <td><img class="img" src="{{$producto->path_imagen_1}}" width="175" height="100"></td>
+            <td>{{$producto->created_at->format('Y-m-d')}}</td>
             <td>{!! Form::open(['method' => 'DELETE','route' => ['producto.destroy', $producto->id],'style'=>'display:inline']) !!}
                 {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
                 {!! Form::close() !!}
             </td>
-            <td>Editar</td>
+            <td><button type="button" class="btn btn-success">Editar</button></td>
         </tr>
     @endforeach
             </tbody>
         </table>
         <div class="paginacion">{{$productos->links()}}</div>
     </div>
+@endsection
+
+@section('javascript')
+    <script src="{{asset('js/admin/helpers.js')}}"></script>
+
+    <script>
+        quitarLaFuncionalidadEnviarDelBotonEliminar();
+    </script>
 @endsection
