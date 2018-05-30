43a775a1e6dd23ccef299e38236726540b09199f
Hanhan Chen <chenhanhan1989@gmail.com>
2018-05-31 00:03:37 +0200

    hacer que la página sea responsive admin

--- resources/views/layout-frontal.blade.php
+++ resources/views/layout-frontal.blade.php
@@ -2,6 +2,7 @@
 <html lang='es'>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> @yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link href="{{ asset('css/comun.css') }}" rel="stylesheet">
