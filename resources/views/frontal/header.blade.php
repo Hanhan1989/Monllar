b7613c149cb04b4397830b05a1e22a255c40190d
Alex Chavez <alex@activitat.com>
2018-05-31 18:58:25 +0200

    #0111 Adaptar tamaño del logo cuando es para pantalla grande

--- resources/views/frontal/header.blade.php
+++ resources/views/frontal/header.blade.php
@@ -1,13 +1,13 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
         
-        <div class="col-3 logo">
+        <div class="col-md-1 col-3 logo">
             <img src="media/images/logo.png" alt="">
         </div>
-        <div class="col-4 text-center spacelogo">
+        <div class="col-md-9 col-4 text-center spacelogo">
             <a class="blog-header-logo text-dark" href="{{url('/')}}">Monllar</a>
         </div>
-        <div class="col-4 justify-content-end align-items-center spacelogo"">
+        <div class="col-md-2 col-4 justify-content-end align-items-center spacelogo">
             <a class="btn btn-sm btn-outline-secondary" href="{{url('login')}}">Login Admin <img class="glyph-icon" src="{{asset('lib/glyph-iconset/svg/si-glyph-sign-in.svg')}}"></a>
         </div>
     </div>
