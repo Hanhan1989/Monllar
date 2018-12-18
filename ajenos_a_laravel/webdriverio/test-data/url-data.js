dcd424d51dc4ab1179b2174e2986d2537f85af44
Hanhan <hanhan@MacBook-Air-de-Hanhan.local>
2018-12-18 22:42:47 +0100

    Ficheros de datos. Es el modelo de la aplicación Selenium

---
+++ ajenos_a_laravel/webdriverio/test-data/url-data.js
@@ -0,0 +1,13 @@
+const base = 'http://local.monllar.com';
+
+export default {
+    
+    get base() {
+        return base;
+    },
+
+    frontend: {
+        login: `${base}/login`
+    }
+   
+};
